aa5520e8da3de547f252f2cc6ec1ca9a02f5108a
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2021-11-01 14:33:37 +0100

    c

--- dllRunner/dllRunner.cpp
+++ dllRunner/dllRunner.cpp
@@ -1,15 +1,6 @@
 #include <windows.h>
 #include <iostream>
 
-/* Define a function pointer for our imported
- * function.
- * This reads as "introduce the new type f_funci as the type:
- *                pointer to a function returning an int and
- *                taking no arguments.
- *
- * Make sure to use matching calling convention (__cdecl, __stdcall, ...)
- * with the exported function. __stdcall is the convention used by the WinAPI
- */
 typedef int(__cdecl* mai)(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
@@ -18,6 +9,7 @@ int main(int argc, char* argv[])
 		std::cerr << "Need DLL ARG" << std::endl;
 		return EXIT_FAILURE;
 	}
+
 	HINSTANCE hGetProcIDDLL = LoadLibraryA(argv[1]);
 
 	if (!hGetProcIDDLL) {
